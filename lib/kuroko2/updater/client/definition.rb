f4c6f88e86f31457e2fd5ab53cd92e48bcc1dab0
Ed Robinson <edward-robinson@cookpad.com>
2019-10-03 16:49:59 +0100

    Fix faraday error
    With a newer version of faraday we are seeing this error:
    
    ```
    Adapter should be set using the `adapter` method, not `use`
    ```

--- lib/kuroko2/updater/client/definition.rb
+++ lib/kuroko2/updater/client/definition.rb
@@ -61,7 +61,7 @@ module Kuroko2
               config.basic_auth(ENV.fetch("KUROKO2_API_USER"), ENV.fetch("KUROKO2_API_KEY"))
               config.use FaradayMiddleware::EncodeJson
               config.use FaradayMiddleware::ParseJson
-              config.use Faraday::Adapter::NetHttp
+              config.adapter Faraday::Adapter::NetHttp
             end
           end
       end
